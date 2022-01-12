790a412c014c9b77fd3516fd8b82475d479dc5ee
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2022-01-12 09:06:49 +0200

    add SearchResultsPage testObjects
    - chx_green
    - chx_packagingLinks
    - chx_plasticBags
    - div_filtersCards
    - div_filterProductType
    - h1_pageHeader
    - h2_subHeader
    - pagination link

---
+++ Object Repository/scenario01/results_page/h2_pageSubHeader.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_pageSubHeader</name>
+   <tag></tag>
+   <elementGuidId>cf3936af-6a33-4574-bfb0-f6654a97f4fc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.product-list-container div.section-subheading > h2</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/scenario01/results_page/span_greenCount.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_greenCount</name>
+   <tag></tag>
+   <elementGuidId>1e990ed9-583f-4d31-84f5-1fd3c641e0e8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@class=&quot;color-green check&quot;]/following-sibling::span</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
