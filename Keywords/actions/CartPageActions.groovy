04d5afdfe425a9b543295c72390e3c20e43be94a
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2022-01-13 14:54:33 +0200

    Update CartPageActions.groovy
    - create function: getCartProductDataByIndex(int index ,String dataSelector)
    - create function: calculateCartProductsTotal()
    - create function: calculateSummaryTotal()
    - create function: getProductFiledBySku(Product product , String dataSelector)

--- Keywords/actions/CartPageActions.groovy
+++ Keywords/actions/CartPageActions.groovy
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement
 import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
+import helpers.CartPageHelpers
 import helpers.GeneralHelpers
 import internal.GlobalVariable
 import models.Product
@@ -35,56 +36,12 @@ public class CartPageActions {
 
 
 	/**
-	 * Get cart product specific data by product index in cart list
-	 * @param dataSelector
-	 * @return TestObject
-	 * @author Eng. Amal Hamad
-	 */
-	public static TestObject getCartProductDataByIndex(int index ,String dataSelector) {
-		List<WebElement> data = WebUI.findWebElements(findTestObject(dataSelector),GlobalVariable.elementVisibilityTimeOut)
-		TestObject object = WebUI.convertWebElementToTestObject(data.get(index))
-		return object
-	}
-
-	/**
-	 * Calculate cart products total
-	 * @return cartProductTotal
-	 * @author Eng. Amal Hamad
-	 */
-	public static String calculateCartProductsTotal() {
-		List<WebElement> productTotalList = WebUI.findWebElements(findTestObject(CartPageActions.productTotal),GlobalVariable.elementVisibilityTimeOut)
-		double total = 0;
-		for(WebElement element: productTotalList) {
-			TestObject productTotal = WebUI.convertWebElementToTestObject(element)
-			total += GeneralHelpers.convertStringToDouble(productTotal)
-		}
-		return GeneralHelpers.formatePrice(total)
-	}
-
-	/**
-	 * Calculate cart products total
-	 * @return cartProductTotal
-	 * @author Eng. Amal Hamad
-	 */
-	public static String calculateSummaryTotal() {
-		List<WebElement> summaryValuesList = WebUI.findWebElements(findTestObject(CartPageActions.summaryValues),GlobalVariable.elementVisibilityTimeOut)
-		double total = 0;
-		for(WebElement element: summaryValuesList) {
-			TestObject productTotal = WebUI.convertWebElementToTestObject(element)
-			if(WebUI.getText(productTotal).contains("[0-9]+")) {
-				total += GeneralHelpers.convertStringToDouble(productTotal)
-			}
-		}
-		return GeneralHelpers.formatePrice(total)
-	}
-
-	/**
 	 * Click on "+" button for this product index
 	 * @param product
 	 * @author Eng. Amal Hamad
 	 */
 	public static void clickPlusQtyButton(Product product) {
-		TestObject btnPlus = CartPageActions.getProductFiledBySku(product , CartPageActions.btnPlusQuantity)
+		TestObject btnPlus = CartPageHelpers.getProductFiledBySku(product , CartPageActions.btnPlusQuantity)
 		WebUI.click(btnPlus)
 	}
 
@@ -94,23 +51,8 @@ public class CartPageActions {
 	 * @author Eng. Amal Hamad
 	 */
 	public static void clickMinusQtyButton(Product product) {
-		TestObject btnMinus = CartPageActions.getProductFiledBySku(product , CartPageActions.btnMinusQuantity)
+		TestObject btnMinus = CartPageHelpers.getProductFiledBySku(product , CartPageActions.btnMinusQuantity)
 		WebUI.click(btnMinus)
-	}
-
-	public static TestObject getProductFiledBySku(Product product , String dataSelector) {
-		String productSku = product.getSku()
-
-		List<WebElement> productSkuList = WebUI.findWebElements(findTestObject(CartPageActions.productSku),GlobalVariable.elementVisibilityTimeOut)
-
-		for (int i = 0 ; i < productSkuList.size() ; i++) {
-			TestObject productSkuTestObject = WebUI.convertWebElementToTestObject(productSkuList.get(i))
-			String currentSku = WebUI.getText(productSkuTestObject)
-			if(currentSku.equals(productSku)) {
-				TestObject testObject = CartPageActions.getCartProductDataByIndex(i , dataSelector)
-				return testObject
-			}
-		}
 	}
 
 }
