74519458e2ad3f648325858b0a06dd3384c97c30
obaidaattaee <obaataii@gmail.com>
2022-01-13 11:14:04 +0200

    verify quick action page and orders

--- Keywords/validations/QuickActionValidations.groovy
+++ Keywords/validations/QuickActionValidations.groovy
@@ -6,6 +6,9 @@ import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
 
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
+
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
 import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
@@ -22,7 +25,55 @@ import internal.GlobalVariable
 
 public class QuickActionValidations {
 
+	/**
+	 * verify quick action active when hover 
+	 * 
+	 * @author selenium
+	 */
 	public static void verifyOnActiveIcon() {
 		assert WebUI.getAttribute(findTestObject('Object Repository/QuickAction/QuickActionIcon'), "class").contains("icon-quick-order")
+	}
+
+	/**
+	 * verify quick action tab 
+	 * 
+	 * @author selenium
+	 */
+	public static void verifyQuickOrderTabText() {
+		assert WebUI.getText(findTestObject('Object Repository/QuickAction/QuicOrderTab')).contains("QUICK\nORDER")
+	}
+
+	/**
+	 * verify default count of quick orders 
+	 * 
+	 * @author selenium
+	 */
+	public static void verifyDefaultQuickItemsCount() {
+		List<WebElement> quickOrders = WebUI.findWebElements(findTestObject('Object Repository/QuickAction/QuickOrders') , GlobalVariable.elementVisibilityTimeOut)
+
+		assert quickOrders.size().equals(5)
+	}
+
+	/**
+	 * verify add more button present in page 
+	 * 
+	 * @author selenium
+	 */
+	public static void verifyAddMoreButtonExist() {
+		assert WebUI.verifyElementPresent(findTestObject('Object Repository/QuickAction/AddMoreOrders'), GlobalVariable.elementVisibilityTimeOut)
+	}
+	
+	/**
+	 * verify SKU input in all orders and that contains default placeholder
+	 * 
+	 * @author selenium
+	 */
+	public static void verifySKUFieldInEveryOrder() {
+		List<WebElement> quickOrders = WebUI.findWebElements(findTestObject('Object Repository/QuickAction/QuickOrders') , GlobalVariable.elementVisibilityTimeOut)
+		
+		for(WebElement order in quickOrders) {
+			assert WebUI.verifyElementPresent(WebUI.convertWebElementToTestObject(order.findElement(By.tagName("input"))) , GlobalVariable.elementVisibilityTimeOut)
+			assert WebUI.getAttribute(WebUI.convertWebElementToTestObject(order.findElement(By.tagName("input"))) , "placeholder").contains("Enter Stock #")
+		}
 	}
 }
---
+++ Object Repository/QuickAction/QuickOrders.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>QuickOrders</name>
+   <tag></tag>
+   <elementGuidId>76ece953-1164-4608-9df2-42ca1b669d46</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>.product-table tbody tr</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
