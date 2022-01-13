54f14f0d7d06efadd42c3b32d20772f0210d5c66
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2022-01-13 21:55:29 +0200

    Update CartPageActions.groovy
    - create function: clickPlusQtyButton(Product product)
    - create function: clickMinusQtyButton(Product product)
    - create function: clickProceedToCheckoutButton()

--- Keywords/actions/CartPageActions.groovy
+++ Keywords/actions/CartPageActions.groovy
@@ -32,27 +32,50 @@ public class CartPageActions {
 	public static String summarySubtotalLabel = 'Object Repository/CartPage/td_summarySubtotalLabel'
 	public static String summarySubtotalValue = 'Object Repository/CartPage/td_summarySubtotalValue'
 	public static String summaryShippingValue = 'Object Repository/CartPage/td_summaryShippingValue'
+	public static String summaryTaxValue = 'Object Repository/CartPage/td_summaryTaxValue'
 	public static String summaryTotalValue = 'Object Repository/CartPage/td_summaryTotalValue'
+	//----------- Checkout
+	public static String buttonProceedToCheckout = 'Object Repository/CartPage/button_proceedToCheckout'
 
 
 	/**
 	 * Click on "+" button for this product index
-	 * @param product
+	 * @param index
 	 * @author Eng. Amal Hamad
 	 */
-	public static void clickPlusQtyButton(Product product) {
-		TestObject btnPlus = CartPageHelpers.getProductFiledBySku(product , CartPageActions.btnPlusQuantity)
+	public static void clickPlusQtyButton(int index) {
+		TestObject btnPlus = CartPageHelpers.getCartProductDataByIndex(index , CartPageActions.btnPlusQuantity)
 		WebUI.click(btnPlus)
 	}
 
 	/**
 	 * Click on "-" button for this product index
-	 * @param product
+	 * @param index
 	 * @author Eng. Amal Hamad
 	 */
-	public static void clickMinusQtyButton(Product product) {
-		TestObject btnMinus = CartPageHelpers.getProductFiledBySku(product , CartPageActions.btnMinusQuantity)
+	public static void clickMinusQtyButton(int index ) {
+		TestObject btnMinus = CartPageHelpers.getCartProductDataByIndex(index, CartPageActions.btnMinusQuantity)
 		WebUI.click(btnMinus)
 	}
+
+	//	public static void clickPlusQtyButton(Product product) {
+	//		TestObject btnPlus = CartPageHelpers.getCartProductDataBySku(product , CartPageActions.btnPlusQuantity)
+	//		WebUI.click(btnPlus)
+	//	}
+	//
+	//	public static void clickMinusQtyButton(Product product) {
+	//		TestObject btnMinus = CartPageHelpers.getCartProductDataBySku(product , CartPageActions.btnMinusQuantity)
+	//		WebUI.click(btnMinus)
+	//	}
+
+	/**
+	 * Click on "ProceedToCheckout" button
+	 * @author Eng. Amal Hamad
+	 */
+	public static void clickProceedToCheckoutButton() {
+		TestObject testObject = findTestObject(CartPageActions.buttonProceedToCheckout)
+		WebUI.click(testObject)
+	}
+
 
 }
