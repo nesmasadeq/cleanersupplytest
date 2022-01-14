42ea745018a6c078c676d7b12c2e9fe76353c873
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2022-01-14 21:39:44 +0200

    Create SearchResultsPageValidations.groovy
    -create function: verifySubHeader()

---
+++ Keywords/validations/SearchResultsPageValidations.groovy
@@ -0,0 +1,35 @@
+package validations
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+import items.SearchResultsPageItems
+import models.AppConstants
+
+public class SearchResultsPageValidations {
+
+	/**
+	 * Verify sub header text match expected text
+	 * @author Eng. Amal Hamad
+	 */
+	public static void verifySubHeader() {
+		System.out.println('subPageHeader: ' + WebUI.getText(SearchResultsPageItems.pageSubHeader))
+		assert WebUI.getText(SearchResultsPageItems.pageSubHeader).toLowerCase().contains(AppConstants.SEARCH_TERM)
+	}
+}
