10b8f3d62ea652d3f47d9d871e03aef15d70b230
amalhamad1250 <93972315+amalhamad95@users.noreply.github.com>
2022-01-14 21:42:48 +0200

    Update ProductsFiltersValidations.groovy
    - create function: verifyAnyFiltersProductsCountMatchResultsCount()

--- Keywords/validations/ProductsFiltersValidations.groovy
+++ Keywords/validations/ProductsFiltersValidations.groovy
@@ -3,16 +3,23 @@ package validations
 
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 
+import java.security.acl.Group
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.webui.driver.DriverFactory
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-import actions.ProductFiltersActions
+import groovy.text.Template
 import helpers.GeneralHelpers
 import internal.GlobalVariable
+import items.HeaderItems
 import items.ProductFiltersItems
+import items.SearchResultsPageItems
 
 
 public class ProductsFiltersValidations {
@@ -27,23 +34,47 @@ public class ProductsFiltersValidations {
 		assert WebUI.getAttribute(testObject, "class").contains("selected")
 	}
 
-
 	/**
 	 * Verify filter products count match results count
 	 * @param filterCount
 	 * @param resultsCount
 	 * @author Eng. Amal Hamad
 	 */
-	public static void verifyFilterProductsCountMatchResultsCount(TestObject filterCount , TestObject resultsCount) {
+	public static void verifyFilterProductsCountMatchResultsCount(TestObject filterCount) {
 		int filterProductsCount = GeneralHelpers.convertStringToInteger(filterCount)
-
-		System.out.println('filterProductsCount: ' + filterProductsCount)
-
-		int productsCountInHeader = GeneralHelpers.convertStringToInteger(resultsCount)
-
-		System.out.println('productsCountInHeader: ' + productsCountInHeader)
-
+		int productsCountInHeader = GeneralHelpers.convertStringToInteger(SearchResultsPageItems.pageSubHeader)
+		System.out.println('filterProductsCount: ' + filterProductsCount + ' ##productsCountInHeader: ' + productsCountInHeader)
 		assert filterProductsCount == productsCountInHeader
+	}
+
+	/**
+	 * Verify filters product count in each group match results count
+	 * @author Eng. Amal Hamad
+	 */
+	public static void verifyAnyFiltersProductsCountMatchResultsCount() {
+		WebDriver driver = DriverFactory.getWebDriver()
+		List<WebElement> filtersMainGroup = driver.findElements(By.cssSelector("div[class*= 'js-fg-']"))
+
+		for(int i = 0 ; i < filtersMainGroup.size() ; i++) {
+			WebElement groupElement = filtersMainGroup.get(i)
+			//Get sub links for this Group
+			List groupLinks = groupElement.findElements(By.cssSelector(".list-unstyled a"))
+			int productsCount = 0
+			for(int j = 0 ; j < groupLinks.size() ; j++) {
+				TestObject currentFilter = WebUI.convertWebElementToTestObject(groupLinks.get(j))
+				//				System.out.println(WebUI.getText(currentFilter))
+				String text = WebUI.getText(currentFilter)
+				String temp =  text.substring(text.indexOf("(")+1, text.indexOf(")"));
+				try {
+					int currentCount  = Integer.parseInt(temp.replaceAll("[^0-9]", ""))
+					productsCount += currentCount
+				}catch(Exception e) {
+					System.out.println( "Exception: " + e.getMessage())
+				}
+
+			}
+			System.out.println(groupElement.getText() + " >> productsCount = " + productsCount)
+		}
 	}
 
 	/**
@@ -52,11 +83,11 @@ public class ProductsFiltersValidations {
 	 * @author Eng. Amal Hamad
 	 */
 	public static void verifySelectedFilters(String... filters) {
-		boolean isSelectedFiltersSectionExist = WebUI.verifyElementPresent(findTestObject(ProductFiltersItems.selectedFilters),GlobalVariable.elementVisibilityTimeOut, FailureHandling.CONTINUE_ON_FAILURE)
+		boolean isSelectedFiltersSectionExist = WebUI.verifyElementPresent(ProductFiltersItems.selectedFilters,GlobalVariable.elementVisibilityTimeOut, FailureHandling.CONTINUE_ON_FAILURE)
 		System.out.println(isSelectedFiltersSectionExist)
 
 		if (isSelectedFiltersSectionExist) {
-			List<WebElement> selectedFilters = WebUI.findWebElements(findTestObject(ProductFiltersItems.selectedFilters),GlobalVariable.elementVisibilityTimeOut)
+			List<WebElement> selectedFilters = WebUI.findWebElements(ProductFiltersItems.selectedFilters,GlobalVariable.elementVisibilityTimeOut)
 			for(int i = 0 ; i<selectedFilters.size() ; i++) {
 				TestObject object = WebUI.convertWebElementToTestObject(selectedFilters.get(i))
 				System.out.println('selectedFilters: ' + WebUI.getText(object))
@@ -71,9 +102,7 @@ public class ProductsFiltersValidations {
 	 * @author Eng. Amal Hamad
 	 */
 	public static void verifyPaginationIsChanged(int size) {
-		List<TestObject> paginationLinks = WebUI.findWebElements(findTestObject('SearchResultPage/a_paginationLinks'),
-				GlobalVariable.elementVisibilityTimeOut)
-
+		List<TestObject> paginationLinks = WebUI.findWebElements(SearchResultsPageItems.paginationLinks, GlobalVariable.elementVisibilityTimeOut)
 		assert paginationLinks.size() == size
 	}
 
