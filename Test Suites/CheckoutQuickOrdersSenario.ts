b1738e63e21aeccb14a380a10386a30c5f9def4c
obaidaattaee <obaataii@gmail.com>
2022-01-15 13:23:32 +0200

    merge master to order_products_collection

---
+++ Test Suites/CheckoutQuickOrdersSenario.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CheckoutQuickOrdersSenario</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>628ba759-0547-425f-9dcc-85d7d04fb2aa</testSuiteGuid>
+   <testCaseLink>
+      <guid>fb607731-2058-4577-9c0b-b9dda64d3d5c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Checkout scenarios/CheckoutWithQuickOrder</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
